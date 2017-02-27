6b58f6a0dba6ca323be5fc6d227fac6cc6a72182
MinaMeltem <meltem@gmail.com>
2017-02-26 21:49:05 -0500

    First Commit

---
+++ app/src/main/java/com/example/hapadim/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.hapadim;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
