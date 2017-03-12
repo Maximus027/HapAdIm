c1f55f5046945491c408d0977cc055ab5c665834
Max Rosado <max.a.rosado@gmail.com>
2017-03-11 19:01:58 -0500

    Created Google Fit Service, still need to test with mock activity and layout.

--- app/src/main/java/com/example/hapadim/WelcomeActivity.java
+++ app/src/main/java/com/example/hapadim/WelcomeActivity.java
@@ -9,13 +9,10 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,57 +21,24 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.Scopes;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.common.api.Scope;
-import com.google.android.gms.common.api.Status;
-import com.google.android.gms.fitness.Fitness;
-import com.google.android.gms.fitness.data.DataPoint;
-import com.google.android.gms.fitness.data.DataSource;
-import com.google.android.gms.fitness.data.DataType;
-import com.google.android.gms.fitness.data.Field;
-import com.google.android.gms.fitness.data.Value;
-import com.google.android.gms.fitness.request.DataSourcesRequest;
-import com.google.android.gms.fitness.request.OnDataPointListener;
-import com.google.android.gms.fitness.request.SensorRequest;
-import com.google.android.gms.fitness.result.DataSourcesResult;
+import com.example.hapadim.googlefit.GoogleFitService;
 
-import java.util.concurrent.TimeUnit;
-
-public class WelcomeActivity extends AppCompatActivity implements OnDataPointListener,
-        GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener {
-
-    private static final String TAG = MainActivity.class.getName();
+public class WelcomeActivity extends AppCompatActivity {
 
     private ViewPager viewPager;
     private MyViewPagerAdapter myViewPagerAdapter;
     private LinearLayout dotsLayout;
     private TextView[] dots;
     private int[] layouts;
-    private Button btnSkip, btnNext;
+    private Button buttonSkip, btnNext;
     private PrefManager prefManager;
-
-    private static final int REQUEST_OAUTH = 1;
-    private static final String AUTH_PENDING = "auth_state_pending";
-    private boolean authInProgress = false;
-    private GoogleApiClient googleApiClient;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Checking for first time launch - before calling setContentView()
         prefManager = new PrefManager(this);
-//        if (!prefManager.isFirstTimeLaunch()) {
-//            launchHomeScreen();
-//            finish();
-//        }
 
         if (Build.VERSION.SDK_INT >= 21) {
             getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
@@ -84,15 +48,14 @@ public class WelcomeActivity extends AppCompatActivity implements OnDataPointLis
 
         viewPager = (ViewPager) findViewById(R.id.view_pager);
         dotsLayout = (LinearLayout) findViewById(R.id.layoutDots);
-        btnSkip = (Button) findViewById(R.id.btn_skip);
+        buttonSkip = (Button) findViewById(R.id.btn_skip);
         btnNext = (Button) findViewById(R.id.btn_next);
 
         layouts = new int[]{
                 R.layout.welcome_side1,
                 R.layout.welcome_side2,
                 R.layout.welcome_side3,
-                R.layout.welcome_side4}
-        ;
+                R.layout.welcome_side4 };
 
         addBottomDots(0);
 
@@ -102,7 +65,7 @@ public class WelcomeActivity extends AppCompatActivity implements OnDataPointLis
         viewPager.setAdapter(myViewPagerAdapter);
         viewPager.addOnPageChangeListener(viewPagerPageChangeListener);
 
-        btnSkip.setOnClickListener(new View.OnClickListener() {
+        buttonSkip.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 launchHomeScreen();
@@ -122,40 +85,11 @@ public class WelcomeActivity extends AppCompatActivity implements OnDataPointLis
                 }
             }
         });
-
-        googleApiClient = new GoogleApiClient.Builder(this)
-                .addApi(Fitness.SENSORS_API)
-                .addScope(new Scope(Scopes.FITNESS_ACTIVITY_READ_WRITE))
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .build();
-
     }
 
-    protected void onStart() {
-        super.onStart();
-        googleApiClient.connect();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-
-        Fitness.SensorsApi.remove( googleApiClient, this )
-                .setResultCallback(new ResultCallback<Status>() {
-                    @Override
-                    public void onResult(Status status) {
-                        if (status.isSuccess()) {
-                            googleApiClient.disconnect();
-                        }
-                    }
-                });
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putBoolean(AUTH_PENDING, authInProgress);
+    private void startGoogleFitService() {
+        Intent intent = new Intent(this, GoogleFitService.class);
+        startService(intent);
     }
 
     private void addBottomDots(int currentPage) {
@@ -196,11 +130,11 @@ public class WelcomeActivity extends AppCompatActivity implements OnDataPointLis
             if (position == layouts.length - 1) {
 
                 btnNext.setText(getString(R.string.start));
-                btnSkip.setVisibility(View.GONE);
+                buttonSkip.setVisibility(View.GONE);
             } else {
 
                 btnNext.setText(getString(R.string.next));
-                btnSkip.setVisibility(View.VISIBLE);
+                buttonSkip.setVisibility(View.VISIBLE);
             }
         }
 
@@ -221,71 +155,6 @@ public class WelcomeActivity extends AppCompatActivity implements OnDataPointLis
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
             window.setStatusBarColor(Color.TRANSPARENT);
         }
-    }
-
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-        DataSourcesRequest dataSourceRequest = new DataSourcesRequest.Builder()
-                .setDataTypes( DataType.TYPE_STEP_COUNT_CUMULATIVE )
-                .setDataSourceTypes( DataSource.TYPE_RAW )
-                .build();
-
-        ResultCallback<DataSourcesResult> dataSourcesResultCallback = new ResultCallback<DataSourcesResult>() {
-            @Override
-            public void onResult(DataSourcesResult dataSourcesResult) {
-                for( DataSource dataSource : dataSourcesResult.getDataSources() ) {
-                    if( DataType.TYPE_STEP_COUNT_CUMULATIVE.equals( dataSource.getDataType() ) ) {
-                        registerFitnessDataListener(dataSource, DataType.TYPE_STEP_COUNT_CUMULATIVE);
-                    }
-                }
-            }
-        };
-
-        Fitness.SensorsApi.findDataSources(googleApiClient, dataSourceRequest)
-                .setResultCallback(dataSourcesResultCallback);
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
-    }
-
-    @Override
-    public void onDataPoint(DataPoint dataPoint) {
-        for( final Field field : dataPoint.getDataType().getFields() ) {
-            final Value value = dataPoint.getValue( field );
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    Toast.makeText(getApplicationContext(), "Field: " + field.getName() + " Value: " + value, Toast.LENGTH_SHORT).show();
-                }
-            });
-        }
-
-    }
-
-    private void registerFitnessDataListener(DataSource dataSource, DataType dataType) {
-
-        SensorRequest request = new SensorRequest.Builder()
-                .setDataSource( dataSource )
-                .setDataType( dataType )
-                .setSamplingRate( 3, TimeUnit.SECONDS )
-                .build();
-
-        Fitness.SensorsApi.add( googleApiClient, request, this )
-                .setResultCallback(new ResultCallback<Status>() {
-                    @Override
-                    public void onResult(Status status) {
-                        if (status.isSuccess()) {
-                            Log.e( "GoogleFit", "SensorApi successfully added" );
-                        }
-                    }
-                });
     }
 
     public class MyViewPagerAdapter extends PagerAdapter {
--- app/src/main/java/com/example/hapadim/googlefit/GoogleFitService.java
+++ app/src/main/java/com/example/hapadim/googlefit/GoogleFitService.java
@@ -1,11 +1,11 @@
 package com.example.hapadim.googlefit;
 
-import android.app.IntentService;
+import android.app.Service;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -16,98 +16,36 @@ import com.google.android.gms.common.api.Scope;
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.fitness.Fitness;
 import com.google.android.gms.fitness.data.DataPoint;
-import com.google.android.gms.fitness.data.DataSet;
 import com.google.android.gms.fitness.data.DataSource;
 import com.google.android.gms.fitness.data.DataType;
 import com.google.android.gms.fitness.data.Field;
-import com.google.android.gms.fitness.request.DataReadRequest;
+import com.google.android.gms.fitness.data.Value;
 import com.google.android.gms.fitness.request.DataSourcesRequest;
 import com.google.android.gms.fitness.request.OnDataPointListener;
 import com.google.android.gms.fitness.request.SensorRequest;
-import com.google.android.gms.fitness.result.DataReadResult;
 import com.google.android.gms.fitness.result.DataSourcesResult;
 
-import java.util.Calendar;
-import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 /**
  * Created by maxrosado on 3/9/17.
  */
 
-public class GoogleFitService extends IntentService implements OnDataPointListener,
+public class GoogleFitService extends Service implements OnDataPointListener,
         GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener{
 
     private static final int REQUEST_OAUTH = 1;
     private static final String AUTH_PENDING = "auth_state_pending";
     private boolean authInProgress = false;
-    private GoogleApiClient googleApiClient = null;
-    private GoogleApiClient.Builder builder;
+    private GoogleApiClient googleApiClient;
 
-    public static final String TAG = "GoogleFitService";
-    private GoogleApiClient mGoogleApiFitnessClient;
-    private boolean mTryingToConnect = false;
 
-    public static final String SERVICE_REQUEST_TYPE = "requestType";
-    public static final int TYPE_GET_STEP_TODAY_DATA = 1;
-    public static final int TYPE_REQUEST_CONNECTION = 2;
-
-    public static final String HISTORY_INTENT = "fitHistory";
-    public static final String HISTORY_EXTRA_STEPS_TODAY = "stepsToday";
-
-    public static final String FIT_NOTIFY_INTENT = "fitStatusUpdateIntent";
-    public static final String FIT_EXTRA_CONNECTION_MESSAGE = "fitFirstConnection";
-    public static final String FIT_EXTRA_NOTIFY_FAILED_STATUS_CODE = "fitExtraFailedStatusCode";
-    public static final String FIT_EXTRA_NOTIFY_FAILED_INTENT = "fitExtraFailedIntent";
-    public static final int TYPE_STOP = 3;
-
-    /**
-     * Creates an IntentService.  Invoked by your subclass's constructor.
-     *
-     * @param name Used to name the worker thread, important only for debugging.
-     */
-    public GoogleFitService(String name) {
-        super(name);
-    }
 
     @Override
-    protected void onHandleIntent(@Nullable Intent intent) {
-        //Get the request type
-        int type = intent.getIntExtra(SERVICE_REQUEST_TYPE, -1);
+    public void onCreate() {
+        super.onCreate();
 
-        //block until google fit connects.  Give up after 10 seconds.
-        if (!mGoogleApiFitnessClient.isConnected()) {
-            mTryingToConnect = true;
-            mGoogleApiFitnessClient.connect();
-
-            //Wait until the service either connects or fails to connect
-            while (mTryingToConnect) {
-                try {
-                    Thread.sleep(100, 0);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-
-        if (mGoogleApiFitnessClient.isConnected()) {
-            if (type == TYPE_GET_STEP_TODAY_DATA) {
-                Log.d(TAG, "Requesting steps from Google Fit");
-                getStepsToday();
-                Log.d(TAG, "Fit update complete.  Allowing Android to destroy the service.");
-            } else if (type == TYPE_REQUEST_CONNECTION) {
-                //Don't need to do anything because the connection is already requested above
-            } else if (type == TYPE_STOP) {
-                mGoogleApiFitnessClient.disconnect();
-            }
-        } else {
-            //Not connected
-            Log.w(TAG, "Fit wasn't able to connect, so the request failed.");
-        }
-    }
-
-    private void buildFitnessClient() {
         googleApiClient = new GoogleApiClient.Builder(this)
                 .addApi(Fitness.SENSORS_API)
                 .addScope(new Scope(Scopes.FITNESS_ACTIVITY_READ_WRITE))
@@ -116,46 +54,10 @@ public class GoogleFitService extends IntentService implements OnDataPointListen
                 .build();
     }
 
-    private void getStepsToday() {
-        Calendar cal = Calendar.getInstance();
-        Date now = new Date();
-        cal.setTime(now);
-        long endTime = cal.getTimeInMillis();
-        cal.set(Calendar.HOUR_OF_DAY, 0);
-        cal.set(Calendar.MINUTE, 0);
-        cal.set(Calendar.SECOND, 0);
-        long startTime = cal.getTimeInMillis();
-
-        final DataReadRequest readRequest = new DataReadRequest.Builder()
-                .read(DataType.TYPE_STEP_COUNT_DELTA)
-                .setTimeRange(startTime, endTime, TimeUnit.MILLISECONDS)
-                .build();
-
-        DataReadResult dataReadResult =
-                Fitness.HistoryApi.readData(mGoogleApiFitnessClient, readRequest).await(1, TimeUnit.MINUTES);
-
-        DataSet stepData = dataReadResult.getDataSet(DataType.TYPE_STEP_COUNT_DELTA);
-
-        int totalSteps = 0;
-
-        for (DataPoint dp : stepData.getDataPoints()) {
-            for(Field field : dp.getDataType().getFields()) {
-                int steps = dp.getValue(field).asInt();
-
-                totalSteps += steps;
-
-            }
-        }
-
-        publishTodaysStepData(totalSteps);
-    }
-
-    private void publishTodaysStepData(int totalSteps) {
-        Intent intent = new Intent(HISTORY_INTENT);
-        // You can also include some extra data.
-        intent.putExtra(HISTORY_EXTRA_STEPS_TODAY, totalSteps);
-
-        LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        googleApiClient.connect();
+        return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
@@ -192,7 +94,16 @@ public class GoogleFitService extends IntentService implements OnDataPointListen
 
     @Override
     public void onDataPoint(DataPoint dataPoint) {
-
+        for( final Field field : dataPoint.getDataType().getFields() ) {
+            final Value value = dataPoint.getValue(field);
+//            Toast.makeText(this, field.getName() + " " + value, Toast.LENGTH_SHORT).show();
+//            runOnUiThread(new Runnable() {
+//                @Override
+//                public void run() {
+//                    Toast.makeText(getApplicationContext(), field.getName() + " " + value, Toast.LENGTH_SHORT).show();
+//                }
+//            });
+        }
     }
 
     private void registerFitnessDataListener(DataSource dataSource, DataType dataType) {
@@ -212,5 +123,11 @@ public class GoogleFitService extends IntentService implements OnDataPointListen
                         }
                     }
                 });
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
     }
 }
