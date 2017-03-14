aeaa839c46d5374f24fe6623e3ae35d959bc3e39
MinaMeltem <meltem@gmail.com>
2017-03-13 21:20:46 -0400

    renaming and setting up landing page

---
+++ app/src/main/java/com/example/hapadim/StartPageActivity.java
@@ -0,0 +1,20 @@
+package com.example.hapadim;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by meltemyildirim on 3/13/17.
+ */
+
+public class StartPageActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_startpage);
+
+
+    }
+}
