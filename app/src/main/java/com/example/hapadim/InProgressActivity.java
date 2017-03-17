dd378c2d0159f983e3f38bd6bf17d7d9f4a349f8
Nesada <knesada@yahoo.com>
2017-03-16 22:18:47 -0400

    Added activity fullscreen

--- app/src/main/java/com/example/hapadim/InProgressActivity.java
+++ app/src/main/java/com/example/hapadim/InProgressActivity.java
@@ -1,8 +1,13 @@
 package com.example.hapadim;
 
 import android.app.Activity;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 /**
  * Created by queenabergen on 3/16/17.
@@ -13,5 +18,19 @@ public class InProgressActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.inprogressscreen);
+
+        toolbarTransparent();
+
     }
+    private void toolbarTransparent(){
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        }
+    }
+
 }
+
--- app/src/main/java/com/example/hapadim/StartPageActivity.java
+++ app/src/main/java/com/example/hapadim/StartPageActivity.java
@@ -1,6 +1,8 @@
 package com.example.hapadim;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.view.ViewPager;
@@ -8,6 +10,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.text.Html;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -83,7 +87,17 @@ public class StartPageActivity extends AppCompatActivity {
         initializeAdapter(example);
         setLocationInformation(example);
 
+        toolbarTransparent();
 
+    }
+    private void toolbarTransparent(){
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        }
     }
 
     private void initializeAdapter(Place example) {
