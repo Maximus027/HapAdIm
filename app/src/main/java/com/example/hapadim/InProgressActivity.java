82bebae0a788858c5d84fb85a2fa4198971b42fc
qbergen <queenabergen@gmail.com>
2017-03-16 20:20:13 -0400

    delete statueofliberty class , updated colors for start screen

--- app/src/main/java/com/example/hapadim/InProgressActivity.java
+++ app/src/main/java/com/example/hapadim/InProgressActivity.java
@@ -1,17 +1,12 @@
 package com.example.hapadim;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.support.annotation.Nullable;
+import com.google.android.youtube.player.YouTubeBaseActivity;
+
 
 /**
  * Created by queenabergen on 3/16/17.
  */
 
-public class InProgressActivity extends Activity {
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.inprogressscreen);
-    }
+public class InProgressActivity extends YouTubeBaseActivity {
+
 }
